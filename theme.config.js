6cc5355190c6f5c63760017a47a8843f636f0a7c
petedavisdev <oldmanplanet@gmail.com>
2022-01-01 01:55:05 +0000

    Initial set up of website

--- theme.config.js
+++ theme.config.js
@@ -2,20 +2,42 @@ const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Pete Davis.
-      <a href="/feed.xml">RSS</a>
-      <style jsx>{`
-        a {
-          float: right;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
-        }
-      `}</style>
-    </small>
+    <footer>
+      <form
+        style={{ display: 'block', marginTop: '8rem' }}
+        action="https://tinyletter.com/redpandatime"
+        method="post"
+        target="popupwindow"
+        onsubmit="window.open('https://tinyletter.com/redpandatime', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true"
+      >
+        <p>
+          <label for="tlemail">
+            Email me when it's red panda time (about once a week)
+          </label>
+        </p>
+        <input
+          style={{ minWidth: '20em', paddingInline: '1em' }}
+          type="email"
+          name="email"
+          id="tlemail"
+          required
+        />
+        <input type="hidden" value="1" name="embed" />
+        <input
+          style={{ paddingInline: '1em' }}
+          type="submit"
+          value="Subscribe"
+        />
+      </form>
+
+      <small style={{ display: 'block', marginTop: '2rem' }}>
+        <a href="/feed.xml">RSS</a> |{' '}
+        <a href="https://www.pinterest.co.uk/redpandatime/redpandatimecom/">
+          Pinterest
+        </a>{' '}
+        | <a href="https://instagram.com/redpandatime">Instagram</a> |{' '}
+        <time>{YEAR}</time> © <a href="https://petedavis.dev">Pete Davis</a>
+      </small>
+    </footer>
   )
 }
