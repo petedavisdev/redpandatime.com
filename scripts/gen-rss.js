48d68caf79db2c839e49b167c5df3107e96d34ce
Pete Davis <7900180+petedavisdev@users.noreply.github.com>
2022-01-08 22:25:16 +0000

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Red Panda Time',
+    site_url: 'https://www.redpandatime.com',
+    feed_url: 'https://www.redpandatime.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
